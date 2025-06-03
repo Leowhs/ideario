856210abf0a185f2bb92eaad2be9f402447bdf40
Leowhs <93284159+Leowhs@users.noreply.github.com>
2025-06-03 18:51:51 -0300

    Add files via upload

--- scripts/java.js
+++ scripts/java.js
@@ -1,15 +1,33 @@
-function clickMenu(){
-            if (itens.style.display == 'block') {
-                itens.style.display = 'none';
-            } else {
-                itens.style.display = 'block';
-            }
-        }
+class MenuIdeario extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <header>
+            <h1><a href="index.html">Ideario</a></h1>
+            <menu>
+                <ul>
+                    <li>
+                        <a href="#" onclick="clickMenu()">
+                            <i class="material-icons">menu</i>
+                            <span>menu</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="material-symbols-outlined">person</span>
+                            <span>conta</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="material-symbols-outlined" id="search">search</span>
+                            <span id="search">buscar</span>
+                        </a>
+                    </li>
+                </ul>
+            </menu>
+        </header>
+        `;
+    }
+}
 
-function mudouTamanho(){
-            if (window.innerWidth >= 768){
-                itens.style.display = 'block';
-            } else {
-                itens.style.display = 'block';
-            }
-        }
+customElements.define('menu-ideario', MenuIdeario);
